036d9b0f4fb4da33b4268ae05503fcb05ee42f65
shun <75880904+shunJS@users.noreply.github.com>
2022-01-04 05:17:48 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ from pymongo import MongoClient
 from datetime import datetime
 
 cluster = MongoClient("mongodb+srv://momo:momo@cluster0.208bb.mongodb.net/myFirstDatabase?retryWrites=true&w=majority")
+
 db = cluster["bakery"]
 users = db["users"]
 orders = db["orders"]
@@ -14,14 +15,15 @@ app = Flask(__name__)
 @app.route("/", methods=["get", "post"])
 def reply():
     text = request.form.get("Body")
-    number = request.form.get("Form")
-    number = number.replace("whatsapp:", "")
+    number = request.form.get("From")
+    number = number.replace("whatsapp:", "")[:-2]
     res = MessagingResponse()
     user = users.find_one({"number": number})
     if bool(user) == False:
-        res.message("Hi, thanks for contacting *The Red Velvet*.\nYou can choose from one of the options below: "
+        msg = res.message("Hi, thanks for contacting *The Red Velvet*.\nYou can choose from one of the options below: "
                     "\n\n*Type*\n\n 1️⃣ To *contact* us \n 2️⃣ To *order* snacks \n 3️⃣ To know our *working hours* \n 4️⃣ "
                     "To get our *address*")
+        msg.media("https://i.ibb.co/BPKnXVP/Red-Velvet-Cake-Waldorf-Astoria.jpg")
         users.insert_one({"number": number, "status": "main", "messages": []})
     elif user["status"] == "main":
         try:
@@ -29,6 +31,7 @@ def reply():
         except:
             res.message("Please enter a valid response")
             return str(res)
+
         if option == 1:
             res.message(
                 "You can contact us through phone or e-mail.\n\n*Phone*: 991234 56789 \n*E-mail* : contact@theredvelvet.io")
@@ -73,8 +76,8 @@ def reply():
             res.message("Please enter a valid response")
     elif user["status"] == "address":
         selected = user["item"]
-        res.message("Thanks for shopping with us!")
-        res.message(f"Your order for {selected} has been received and will be delivered within an hour")
+        res.message("Thanks for shopping with us 😊")
+        res.message(f"Your order for *{selected}* has been received and will be delivered within an hour")
         orders.insert_one({"number": number, "item": selected, "address": text, "order_time": datetime.now()})
         users.update_one(
             {"number": number}, {"$set": {"status": "ordered"}})
@@ -84,7 +87,6 @@ def reply():
                     "To get our *address*")
         users.update_one(
             {"number": number}, {"$set": {"status": "main"}})
-
     users.update_one({"number": number}, {"$push": {"messages": {"text": text, "date": datetime.now()}}})
     return str(res)
 
